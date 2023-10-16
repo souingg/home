0d2d2673c374f7f8b03010baebab4fd69d5194c9
souingg <tranthidiemsuong9112003@gmail.com>
2023-10-16 19:27:40 +0700

    home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProductComponent } from './product/product.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
+    ProductComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
